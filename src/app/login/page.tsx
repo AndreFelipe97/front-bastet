ebea89cf10eedffc51348ecea5c4c46ec9fef610
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2024-05-27 11:42:24 -0300

    feat: integration login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,47 @@
+"use client"
+
+import { api } from "@/services/api";
 import Link from "next/link";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Page() {
+  const [error, setErro] = useState<boolean>(false);
+
+  const router = useRouter();
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    try {
+
+      const email = (event.target as HTMLFormElement).email.value;
+      const password = (event.target as HTMLFormElement).password.value;
+      const response = await api.post('/auth', { email, password });
+      const { sub } = jwtDecode(response.data.token);
+  
+      localStorage.setItem('TOKEN', response.data.token);
+      localStorage.setItem('USER_ID', sub || '');
+      router.push('/');
+    } catch(e) {
+      setErro(true);
+    }
+  }
+
   return (
     <main>      
-      <form className="p-6 bg-indigo-50 max-w-96 rounded-3xl flex flex-col gap-4">
+      <form
+        onSubmit={handleSubmit}
+        className="p-6 bg-indigo-50 max-w-96 rounded-3xl flex flex-col gap-4"
+      >
         <h2 className="page-title">Login</h2>
+        {error && <p className="text-red-500">E-mail ou senha inválidos</p>}
         <div className="flex flex-col gap-2">
           <label htmlFor="email">E-mail</label>
           <input type="email" required name="email" id="email" className="border h-10 rounded-xl focus:outline-none focus:border-indigo-300 px-4 py-2" />
         </div>
         <div className="flex flex-col gap-2">
-          <label htmlFor="senha">Senha</label>
-          <input type="password" required name="senha" id="senha" className="border h-10 rounded-xl focus:outline-none focus:border-indigo-300 px-4 py-2" />
+          <label htmlFor="password">Senha</label>
+          <input type="password" required name="password" id="password" className="border h-10 rounded-xl focus:outline-none focus:border-indigo-300 px-4 py-2" />
         </div>
         <div className="flex flex-row justify-between items-end">
           <Link href="/cadastro" className="my-3">Fazer cadastro</Link>
